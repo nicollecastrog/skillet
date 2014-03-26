be46cad1df3aa48dc57fdbec085626630457283f
Nicolle Castro <nicollecastrog@gmail.com>
2014-03-26 16:58:48 +0000

    trying to fix pentagon issue

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,4 +1,9 @@
+function showForm(parent) { 
+    $(".approval_form", parent).removeClass('hidden');
+  }
+
 $( document ).ready(function() {
+  // setTimeout(function() { showForm($('.feed_item:nth-child(2)'));}, 2000);
 
   $(".less_more").click(function() {
     var parent = $(this).closest('.tier_item');
@@ -18,14 +23,15 @@ $( document ).ready(function() {
     $(".tier_submit_form").addClass("hidden");
   });
 
-  $(".approval").click(function() {
-    var parent = $(this).closest('.feed_textbox');
-    $(".approval_form",parent).removeClass("hidden");
+  $(".approval").click(function(ev) {
+    // $(".approval_form").hide();
+    var parent = $(this).closest('.feed_item');
+    showForm(parent);
   });
 
-  $(".close_approval_form").click(function() {
-    var parent = $(this).closest('.feed_textbox');
-    $(".approval_form",parent).addClass("hidden");
+  $(".close_approval_form").click(function(ev) {
+    var parent = $(this).closest('.feed_item');
+    $(".approval_form", parent).hide();
   });
 
 }); //end document ready
