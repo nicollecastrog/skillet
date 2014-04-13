e3ab1ff755c7ee05f01a89e2bb5f5d3dd43a2ba0
Nicolle Castro <nicollecastrog@gmail.com>
2014-04-13 20:39:02 +0100

    Making errors pretty. Example: error when user tries to approve own work.

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ if defined?(Bundler)
   # Bundler.require(:default, :assets, Rails.env)
 end
 
+
 module Skillet
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -58,5 +59,13 @@ module Skillet
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.to_prepare do
+      Devise::SessionsController.layout "devise"
+      Devise::RegistrationsController.layout proc{ |controller| user_signed_in? ? "application" : "devise" }
+      Devise::ConfirmationsController.layout "devise"
+      Devise::UnlocksController.layout "devise"            
+      Devise::PasswordsController.layout "devise"        
+    end
   end
 end
