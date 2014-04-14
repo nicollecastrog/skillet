4a6b1f3e0e1428c2412dacd90d508b00ee01e311
Nicolle Castro <nicollecastrog@gmail.com>
2014-04-14 19:43:58 +0100

    Still trying to configure AWS3 Carrierwave Heroku

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,3 @@
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    :provider               => 'AWS',
-    :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],
-    :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY']
-  }
-  config.fog_directory  = ENV['S3_BUCKET_NAME']
-end
-
 # config/initializers/carrierwave.rb
 CarrierWave.configure do |config|
   config.root = Rails.root.join('tmp') # adding these...
