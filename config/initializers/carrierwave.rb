7ddd06a0442151e6fc4cb872a7686e669cb86d71
Nicolle Castro <nicollecastrog@gmail.com>
2014-04-14 20:17:02 +0100

    carrierwave changes, removed host

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,9 +4,7 @@ CarrierWave.configure do |config|
     :provider               => 'AWS',                        # required
     :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],                        # required
     :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],                     # required
-    :region                 => 'eu-west-1',                  # optional, defaults to 'us-east-1'
-    :host                   => 'https://s3-eu-west-1.amazonaws.com',             # optional, defaults to nil
-    :endpoint               => 'https://skilletapp.s3-eu-west-1.amazonaws.com' # optional, defaults to nil
+    :region                 => 'eu-west-1'                 # optional, defaults to 'us-east-1'
   }
   # For testing, upload files to local `tmp` folder.
   if Rails.env.test? || Rails.env.cucumber?
@@ -18,7 +16,5 @@ CarrierWave.configure do |config|
   end
  
   config.cache_dir = "#{Rails.root}/tmp/uploads"    # To let CarrierWave work on heroku
-  config.fog_directory    = ENV['S3_BUCKET_NAME']
-  config.s3_access_policy = :public_read     # optional, defaults to true
-  
+  config.fog_directory = ENV['S3_BUCKET_NAME']  
 end
