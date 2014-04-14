45227e4c209b691075443229e42e2d3b5f3e2adb
Nicolle Castro <nicollecastrog@gmail.com>
2014-04-14 20:05:00 +0100

    Fixing carrierwave.rb configurations

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,14 +1,24 @@
 # config/initializers/carrierwave.rb
 CarrierWave.configure do |config|
-  config.root = Rails.root.join('tmp') # adding these...
-  config.cache_dir = 'carrierwave' # ...two lines
-
   config.fog_credentials = {
     :provider               => 'AWS',                        # required
     :aws_access_key_id      => ENV['AWS_ACCESS_KEY_ID'],                        # required
     :aws_secret_access_key  => ENV['AWS_SECRET_ACCESS_KEY'],                     # required
     :region                 => 'eu-west-1',                  # optional, defaults to 'us-east-1'
+    :host                   => 'https://s3-eu-west-1.amazonaws.com',             # optional, defaults to nil
+    :endpoint               => 'https://skilletapp.s3-eu-west-1.amazonaws.com' # optional, defaults to nil
   }
-  config.fog_directory  = 'directory'                             # required
-  config.fog_public     = false                                   # optional, defaults to true
+  # For testing, upload files to local `tmp` folder.
+  if Rails.env.test? || Rails.env.cucumber?
+    config.storage = :file
+    config.enable_processing = false
+    config.root = "#{Rails.root}/tmp"
+  else
+    config.storage = :fog
+  end
+ 
+  config.cache_dir = "#{Rails.root}/tmp/uploads"    # To let CarrierWave work on heroku
+  config.fog_directory    = ENV['S3_BUCKET_NAME']
+  config.s3_access_policy = :public_read     # optional, defaults to true
+  
 end
